174a1c2a4cead1d42697602f4ac5f940a4b18d13
ghkdth9753 <ghkdth9753@naver.com>
2022-06-23 14:36:21 +0900

    pcw_003

--- pcw/js/joincheck.js
+++ pcw/js/joincheck.js
@@ -1,32 +1,8 @@
-function check_pw(){  //비밀번호 확인 
-    var p = document.getElementById('pw').value; 
-    var p_cf = document.getElementById('pw_cf').value; 
- 
-    if (p!=p_cf) { 
-      document.getElementById('pw_check_msg').innerHTML = "비밀번호가 다릅니다. 다시 확인해 주세요."; 
-    } 
-    else { 
-        document.getElementById('pw_check_msg').innerHTML = ""; 
-    } 
-    if (p_cf=="") { 
-      document.getElementById('pw_check_msg').innerHTML = ""; 
-    } 
- }
-
-
-
 function checked(){  //form 유효성 검사 
     var p = document.getElementById('pw'); 
     var p_cf = document.getElementById('pw_cf'); 
     var nameCheck = document.getElementById("name");
     var named= RegExp(/^[가-힣]+$/)
-
-
- if (p.value != p_cf.value) {  //비밀번호 확인 
-    alert("비밀번호가 일치하지 않습니다. 확인해 주세요"); 
-    p_cf.focus(); 
-    return false; 
- }
 
 //이름 유효성 검사
 if(!named.test(nameCheck.value)){
---
+++ pcw/js/resetPass.js
@@ -0,0 +1,29 @@
+function check_pw(){  //비밀번호 확인 
+    var p = document.getElementById('pw').value; 
+    var p_cf = document.getElementById('pw_cf').value; 
+ 
+    if (p!=p_cf) { 
+      document.getElementById('pw_check_msg').innerHTML = "비밀번호가 다릅니다. 다시 확인해 주세요."; 
+    } 
+    else { 
+        document.getElementById('pw_check_msg').innerHTML = ""; 
+    } 
+    if (p_cf=="") { 
+      document.getElementById('pw_check_msg').innerHTML = ""; 
+    } 
+}
+
+function checked(){  //form 유효성 검사 
+    var p = document.getElementById('pw'); 
+    var p_cf = document.getElementById('pw_cf'); 
+
+if (p.value != p_cf.value) {  //비밀번호 확인 
+    alert("비밀번호가 일치하지 않습니다. 확인해 주세요"); 
+    p_cf.focus(); 
+    return false; 
+ }
+
+else { 
+    return true; 
+ } 
+}
